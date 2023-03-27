06da2c230020e21414f0b1839f2131831fdc8474
Adryan r <forget.f255@gmail.com>
2023-03-27 19:22:21 -0300

    Deleta arquivos
    Deleta arquivos que não serão mais usados pelo programa

--- jogos simples/tic-tac-toe.Inc/escolhaXouY.py
+++
@@ -1,6 +0,0 @@
-class PlayChoice():
-    def __init__(self) -> None:
-        self.playChoice = int(input("Escolha uma casa entre 1 e 9: "))
-    
-    def userChoice(self):
-        return self.playChoice
