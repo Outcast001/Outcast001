a3f95365de66abc074849b13be34f8b8541ffd96
Adryan r <forget.f255@gmail.com>
2023-05-01 15:34:06 -0300

    Altera método 'printTable' da classe 'Tabela'

--- jogos simples/tic-tac-toe.Inc/tabela.py
+++ jogos simples/tic-tac-toe.Inc/tabela.py
@@ -1,16 +1,14 @@
-# Casas do tabuleiro.
 class Tabela():
     """ Uma classe que imprime a tabela do jogo tic-tac-toe. """
+    table = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
 
-    def __init__(self, table) -> None:
+    def __init__(self, table):
         self.table = table
 
     def printTable(self):
         """ Como o tabuleiro se parece """
-        print(self.table[0], end='\r')
-        print()
-        print(self.table[1], end='\r')
-        print()
+        print(self.table[0])
+        print(self.table[1])
         print(self.table[2])
-        print()
-        
